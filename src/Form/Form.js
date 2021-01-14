804de5aa51f3251947fa8237bfc57e5936583ef3
Peyton Jones <peytonj.dev@gmail.com>
2021-01-14 15:35:14 -0700

    create form layout

---
+++ src/Form/Form.js
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import { Component } from 'react'
+
+// class Form extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       input: ''
+//     }
+//   }
+
+//   handleChange = e => {
+//     this.setState({ [e.target.name]: e.target.value });
+//   }
+
+//   render() {
+//     return (
+//       <form>
+//         <input placeholder="Leta" name='input'></input>
+//         <button className="submit-btn" type="submit" value="Submit">Submit</button>
+//       </form>
+//     )
+//   }
+// }
+
+// export default Form;
