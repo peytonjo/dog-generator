6dab55186e33a8b44b1f70a8cfd7c6cc55c76704
Peyton Jones <peytonj.dev@gmail.com>
2021-01-14 16:53:11 -0700

    create layout for App

--- src/App/App.js
+++ src/App/App.js
@@ -1,42 +1,51 @@
-import './App.css';
-import { Component } from 'react'
-import { Switch, Route, Link, Router } from "react-router-dom"
-import NavBar from '../NavBar/NavBar'
+import './App.scss';
+import React from 'react'
+import { Component } from 'react';
+import { Switch, Route, Link, Router } from "react-router-dom";
+import NavBar from '../NavBar/NavBar';
+// import Form from '../Form/Form';
+import grassImg from '../grass.png'
+
 
 class App extends Component {
-  Constructor {
-  super()
-  this.state = {
-    displayed: false
+  constructor() {
+    super();
+    this.state = {
+      displayed: false
+    }
   }
-}
 
-displayedHomeBtn = (boolean) => {
-  this.setState({ displayed: boolean })
-}
+  displayedHomeBtn = (boolean) => {
+    this.setState({ displayed: boolean })
+  }
 
 
-render() {
-  return (
-    <div className="App">
-      <main className="main-page">
-        <nav>
-          <NavBar />
-        </nav>
-        <Switch>
-          <Route
-            exact
-            path='/'></Route>
-          <Form />
-          <Route
-            exact
-            path='/user-page'>
-            <User />
-          </Route>
-        </Switch>
-      </main>
-    </div>
-  )
+  render() {
+    return (
+      <div className="app">
+        <main className="main-page">
+          <nav>
+            <NavBar />
+          </nav>
+          <article>
+            <section className="home-view">
+              <img className="dog-img"/>
+              <button className="next-btn page-btns">next</button>
+              <div>
+                <h3>Have you become a dog person?</h3>
+                <button className="answr-btn page-btns">yes</button>
+                <button className="answr-btn page-btns">no</button>
+              </div>
+            </section>
+            
+          </article>
+          <img 
+            className="grass-img"
+            src={grassImg}/>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
