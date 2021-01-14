0bd28527d84e557b68199637443c2d25645d1934
Peyton Jones <peytonj.dev@gmail.com>
2021-01-14 11:06:42 -0700

    create layout for App and its routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,42 @@
 import './App.css';
+import { Component } from 'react'
+import { Switch, Route, Link, Router } from "react-router-dom"
+import NavBar from '../NavBar/NavBar'
 
-function App() {
+class App extends Component {
+  Constructor {
+  super()
+  this.state = {
+    displayed: false
+  }
+}
+
+displayedHomeBtn = (boolean) => {
+  this.setState({ displayed: boolean })
+}
+
+
+render() {
   return (
     <div className="App">
-      <main></main>  
+      <main className="main-page">
+        <nav>
+          <NavBar />
+        </nav>
+        <Switch>
+          <Route
+            exact
+            path='/'></Route>
+          <Form />
+          <Route
+            exact
+            path='/user-page'>
+            <User />
+          </Route>
+        </Switch>
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
