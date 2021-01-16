fc2a2aee9f198f4262dd757f8f6642bdec0b08c1
Peyton Jones <peytonj.dev@gmail.com>
2021-01-16 12:23:11 -0700

    add routes and imports to app for page views

--- src/App/App.js
+++ src/App/App.js
@@ -3,20 +3,43 @@ import React from 'react'
 import { Component } from 'react';
 import { Switch, Route, Link, Router } from "react-router-dom";
 import NavBar from '../NavBar/NavBar';
-// import Form from '../Form/Form';
-import grassImg from '../grass.png'
+import RandomDog from '../RandomDog/RandomDog';
+import User from '../User/User'
+import Form from '../Form/Form'
+import { getRandomDog } from '../ApiCalls';
+import grassImg from '../grass.png';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      image: '',
+      status: '',
       displayed: false
     }
   }
 
   displayedHomeBtn = (boolean) => {
     this.setState({ displayed: boolean })
+  }
+
+  componentDidMount() {
+    this.loadDogImg()
+  }
+
+  loadDogImg = () => {
+    getRandomDog()
+      .then(data => this.setState({ image: data.message }))
+      .catch(data => this.setState({ status: data.status }))
+  }
+
+  congratsAlert = () => {
+    window.alert('WHOOOO HOOOOO');
+  }
+
+  ashamedAlert = () => {
+    window.alert('BOOOOOOOO....try again')
   }
 
 
@@ -27,18 +50,35 @@ class App extends Component {
           <nav>
             <NavBar />
           </nav>
-          <article>
-            <section className="home-view">
-              <img className="dog-img"/>
-              <button className="next-btn page-btns">next</button>
-              <div>
-                <h3>Have you become a dog person?</h3>
-                <button className="answr-btn page-btns">yes</button>
-                <button className="answr-btn page-btns">no</button>
-              </div>
-            </section>
-            
-          </article>
+          <Switch>
+            <Route
+             exact
+             path='/'
+             render={() => {
+               return (
+                <RandomDog image={this.state.image} loadDogImg={this.loadDogImg} congratsAlert={this.congratsAlert}/>
+               )
+             }}>
+            </Route>
+            <Route 
+              exact
+              path="/user-favorites" 
+              render={() => {
+                return (
+                  <User />
+                )
+              }}>
+            </Route>  
+            <Route 
+              exact
+              path="/form-view" 
+              render={() => {
+                return (
+                  <Form />
+                )
+              }}>
+            </Route>       
+          </Switch>         
           <img 
             className="grass-img"
             src={grassImg}/>
