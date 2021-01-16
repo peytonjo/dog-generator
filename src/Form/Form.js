eb65fdf7533f7fc023162cd938ffa7d6b24e7fbd
Peyton Jones <peytonj.dev@gmail.com>
2021-01-16 12:20:42 -0700

    create functionality and styling for formv view

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,26 +1,29 @@
-// import React from 'react'
-// import { Component } from 'react'
+import React from 'react'
+import { Component } from 'react'
+import '../Form/Form.scss'
 
-// class Form extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       input: ''
-//     }
-//   }
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
 
-//   handleChange = e => {
-//     this.setState({ [e.target.name]: e.target.value });
-//   }
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-//   render() {
-//     return (
-//       <form>
-//         <input placeholder="Leta" name='input'></input>
-//         <button className="submit-btn" type="submit" value="Submit">Submit</button>
-//       </form>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <form className="form">
+        <h1>Congrats on leaving the dark side and becoming a dog person!!!!!</h1>
+        <h2>Please enter your name below!</h2>
+        <input placeholder="Leta" name='input' alt="name input box" value={this.state.input} onChange={e => this.handleInput(e)}></input>
+        <button className="submit-btn" type="submit" value="Submit">Submit</button>
+      </form>
+    )
+  }
+}
 
-// export default Form;
+export default Form;
