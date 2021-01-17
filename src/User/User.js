db082f67cebd39a0a4ef682c184b9e30b6a9b6fc
Peyton Jones <peytonj.dev@gmail.com>
2021-01-17 12:30:12 -0700

    complete user view

--- src/User/User.js
+++ src/User/User.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getFavoriteDogs } from '../localStorage'
+import './User.scss'
 
 const User = (props) => {
-
   return (
     <section className="user-view">
       <h1>Your Favorite Dogs:</h1>
-      <div className="fav-dogs"></div>
+      <div className="fav-dogs">{props.favoriteDogs.map(dogData => (
+        <img key={dogData.id} src={dogData.image} />
+      )) }</div>
     </section>
-
-  )
+  );
 }
 
 export default User;
