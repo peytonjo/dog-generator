c1cf271d33a79515ea21a6905d538537d970ed14
Peyton Jones <peytonj.dev@gmail.com>
2021-01-17 12:30:28 -0700

    add dog to local storage on click

--- src/RandomDog/RandomDog.js
+++ src/RandomDog/RandomDog.js
@@ -3,44 +3,54 @@ import React, { Component } from 'react'
 import heartIcon from '../heart.png'
 import { NavLink } from 'react-router-dom'
 import './RandomDog.scss'
+import { getFavoriteDogs } from '../localStorage';
 
 //this component will display a random image on the screen 
 //hold functionality to add image to loacl storage 
 
-const RandomDog = (props) => {
-  let image = props.image
+class RandomDog extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  // addFavDog = (e) => {
-  //   const favDog = {
-  //     id: Date.now(),
-  //     image: '',
-  //   }
-  //     const favoritedDogs = JSON.parse(localStorage.getItem('favoriteDogs'))
-  //     favoritedDogs.push(favDog)
-  //     localStorage.setItem('favoriteDogs', JSON.stringify(favoritedDogs))
-  // }
 
-  return (
-    <section className="home-view">
-      <img className="dog-img" alt="dog photo" src={image}/>
-      <button className="fav-btn">
-        <img
-          className="fav-icon"
-          alt="favorite icon"
-          src={heartIcon}
-          role="button"
-        />
-      </button>
-      <button className="next-btn page-btns" onClick={props.loadDogImg}>next</button>
-      <div>
-        <h3>Have you become a dog person yet?</h3>
-        <NavLink to="/form-view">
-          <button className="answr-btn page-btns" onClick={props.congratsAlert}>yes</button>
-        </NavLink>
-        <button className="answr-btn page-btns">no</button>
-      </div>
-    </section>
-  )
+  addFavDog = (e) => {
+    const favDog = {
+      id: Date.now(),
+      image: this.props.image,
+    }
+    let favoritedDogs = getFavoriteDogs()
+    console.log('favoritedDogs', favoritedDogs)
+    favoritedDogs ? favoritedDogs.push(favDog) : favoritedDogs = [favDog]
+
+    localStorage.setItem('favoriteDogs', JSON.stringify(favoritedDogs))
+    this.props.setFavoriteDogs(favoritedDogs)
+  }
+
+  render() {
+    return (
+      <section className="home-view">
+        <img className="dog-img" alt="dog photo" src={this.props.image} />
+        <button className="fav-btn">
+          <img
+            className="fav-icon"
+            alt="favorite icon"
+            src={heartIcon}
+            role="button"
+            onClick={this.addFavDog}
+          />
+        </button>
+        <button className="next-btn page-btns" onClick={this.props.loadDogImg}>next</button>
+        <div>
+          <h3>Have you become a dog person yet?</h3>
+          <NavLink to="/form-view">
+            <button className="answr-btn page-btns" onClick={this.props.congratsAlert}>yes</button>
+          </NavLink>
+          <button className="answr-btn page-btns">no</button>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default RandomDog;
