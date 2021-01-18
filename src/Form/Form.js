926da62a3e885d44e17ff1eb1decece47f6eb8ed
Peyton Jones <peytonj.dev@gmail.com>
2021-01-18 11:19:52 -0700

    complete styling for form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 import '../Form/Form.scss'
+import confettiImg from '../confetti.png'
 
-class Form extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: ''
-    }
-  }
 
-  handleInput = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+//create function in app 
+//function should take in the inputs value and set it to state 
+//function should be passed through props to the form
+//form should have a conditional render based on the Apps states
+//if the state is a empty string render personal message 
+//else render default message
 
-  render() {
-    return (
-      <form className="form">
-        <h1>Congrats on leaving the dark side and becoming a dog person!!!!!</h1>
-        <h2>Please enter your name below!</h2>
-        <input placeholder="Leta" name='input' alt="name input box" value={this.state.input} onChange={e => this.handleInput(e)}></input>
+const Form = (props) => {
+
+  return (
+    <form className="form">
+      <h1>Congrats on leaving the dark side and becoming a dog person!!!!!</h1>
+      <img src={confettiImg} alt="confetti icon" className="confetti-icon"/>
+      <h2>Please enter your name below!</h2>
+      <input placeholder="Leta" name='input' alt="name input box" onChange={e => props.setName(e.target.value)}></input>
+      <NavLink to="/user-favorites">
         <button className="submit-btn" type="submit" value="Submit">Submit</button>
-      </form>
-    )
-  }
+      </NavLink>
+    </form>
+  )  
 }
 
 export default Form;
